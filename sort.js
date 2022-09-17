95b44fff6ea50347d6d21f8fc119d3244e3f6a7b
chineseyouthculture <110559919+chineseyouthculture@users.noreply.github.com>
2022-09-17 17:33:49 -0400

    Add files via upload

--- sort.js
+++ sort.js
@@ -1,90 +1,47 @@
-(function() {
-		
-    let field = document.querySelector('.items');
-    let li = Array.from(field.children);
+const allFilterItems = document.querySelectorAll('.filter-item');
+const allFilterBtns = document.querySelectorAll('.filter-btn');
 
-    function FilterProduct() {
-        for(let i of li){
-            const name = i.querySelector('strong');
-            const x = name.textContent;
-            i.setAttribute("data-category", x);
-        }
+window.addEventListener('DOMContentLoaded', () => {
+    allFilterBtns[0].classList.add('active-btn');
+});
 
-        let indicator = document.querySelector('.indicator').children;
+allFilterBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        showFilteredContent(btn);
+    });
+});
 
-        this.run = function() {
-            for(let i=0; i<indicator.length; i++)
-            {
-                indicator[i].onclick = function () {
-                    for(let x=0; x<indicator.length; x++)
-                    {
-                        indicator[x].classList.remove('active');
-                    }
-                    this.classList.add('active');
-                    const displayItems = this.getAttribute('data-filter');
+function showFilteredContent(btn){
+    allFilterItems.forEach((item) => {
+        if(item.classList.contains(btn.id)){
+            resetActiveBtn();
+            btn.classList.add('active-btn');
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
 
-                    for(let z=0; z<li.length; z++)
-                    {
-                        li[z].style.transform = "scale(0)";
-                        setTimeout(()=>{
-                            li[z].style.display = "none";
-                        }, 500);
+function resetActiveBtn(){
+    allFilterBtns.forEach((btn) => {
+        btn.classList.remove('active-btn');
+    });
+}
+const search = document.getElementById("search");
+const productName = document.querySelectorAll(".product p");
 
-                        if ((li[z].getAttribute('data-category') == displayItems) || displayItems == "all")
-                         {
-                             li[z].style.transform = "scale(1)";
-                             setTimeout(()=>{
-                                li[z].style.display = "block";
-                            }, 500);
-                         }
-                    }
-                };
-            }
-        }
-    }
+search.addEventListener("keyup", filterProducts);
 
-    function SortProduct() {
-        let select = document.getElementById('select');
-        let ar = [];
-        for(let i of li){
-            const last = i.lastElementChild;
-            const x = last.textContent.trim();
-            const y = Number(x.substring(1));
-            i.setAttribute("data-price", y);
-            ar.push(i);
+function filterProducts(e) {
+    const text = e.target.value.toLowerCase();
+    productName.forEach(function(product) {
+        const item = product.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            item.style.display = "block";
+            console.log(0);
+        } else {
+            item.style.display = "none";
         }
-        this.run = ()=>{
-            addevent();
-        }
-        function addevent(){
-            select.onchange = sortingValue;
-        }
-        function sortingValue(){
-        
-            if (this.value === 'Default') {
-                while (field.firstChild) {field.removeChild(field.firstChild);}
-                field.append(...ar);	
-            }
-            if (this.value === 'LowToHigh') {
-                SortElem(field, li, true)
-            }
-            if (this.value === 'HighToLow') {
-                SortElem(field, li, false)
-            }
-        }
-        function SortElem(field,li, asc){
-            let  dm, sortli;
-            dm = asc ? 1 : -1;
-            sortli = li.sort((a, b)=>{
-                const ax = a.getAttribute('data-price');
-                const bx = b.getAttribute('data-price');
-                return ax > bx ? (1*dm) : (-1*dm);
-            });
-             while (field.firstChild) {field.removeChild(field.firstChild);}
-             field.append(...sortli);	
-        }
-    }
-
-    new FilterProduct().run();
-    new SortProduct().run();
-})();
+    })
+}
